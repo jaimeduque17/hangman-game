60d4468db42d6f1f5405faa74471ed259a4cd388
Jaime Duque <jaime.duque@estratek.com.co>
2020-09-21 14:37:53 -0500

    hangman game

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+// decorador: le agrega funcionalidades a la clase de forma externa
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+
+  palabra = 'AGUACATE';
+  palabraOculta = '';
+  intentos = 0;
+  gano = false;
+  perdio = false;
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor() {
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+  }
+
+  comprobar(letra) {
+    this.existeLetra(letra);
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
+  }
+
+  verificaGane() {
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+    if (palabraEvaluar === this.palabra) {
+      this.gano = true;
+    }
+    if(this.intentos >= 9) {
+      this.perdio = true;
+    }
+  }
+
+  existeLetra(letra) {
+    if (this.palabra.indexOf(letra) >= 0) {
+      // encontró la letra
+      console.log('existe');
+    } else {
+      console.log('no existe');
+      this.intentos++;
+    }
+  }
+}
